8a3762a5f6fd1fdebc5ad6c99b9a11b20498799b
mosespace <kisakyemoses50@gmail.com>
2023-09-18 10:52:21 +0300

    feat: initial commit

--- constants/SstQuestions.js
+++ constants/SstQuestions.js
@@ -1,33 +1,54 @@
 const questions = [
   {
     id: 1,
-    question: "What is the chemical symbol for water?",
-    options: ["A: Wt", "B: Wa", "C: H2O", "D: O2H"],
-    correct_option: "C: H2O",
+    question: "Our environment is made up of?",
+    options: [
+      "A: Plants and animals",
+      "B: Roads and rivers",
+      "C: Natural and man-made-things",
+      "D: Vegetation and climate",
+    ],
+    correct_option: "D: Vegetation and climate",
   },
   {
     id: 2,
-    question: "Which gas do plants absorb from the atmosphere?",
-    options: ["A: Oxygen", "B: Carbon Dioxide", "C: Nitrogen", "D: Methane"],
-    correct_option: "B: Carbon Dioxide",
+    question:
+      "Protection of the environment in Ghana must be the responsibility of?",
+    options: [
+      "A: The government",
+      "B: Environment protection agency",
+      "C: The forestry department",
+      "D: The department of game and wildlife",
+    ],
+    correct_option: "B: Environment protection agency",
   },
   {
     id: 3,
-    question: "What is the smallest planet in our solar system?",
-    options: ["A: Earth", "B: Venus", "C: Mars", "D: Mercury"],
-    correct_option: "D: Mercury",
+    question: "The first Ghanaian member of the legislative council was?",
+    options: [
+      "A: James Kwegyir Aggrey",
+      "B: George Kuntu Blankson",
+      "C: John Sey",
+      "D: king Aggrey",
+    ],
+    correct_option: "B: George Kuntu Blankson",
   },
   {
     id: 4,
-    question: "Which gas makes up the majority of Earth's atmosphere?",
-    options: ["A: Oxygen", "B: Carbon Dioxide", "C: Nitrogen", "D: Hydrogen"],
-    correct_option: "C: Nitrogen",
+    question: "The main duty of the police in a community is to?",
+    options: [
+      "A: Arrest criminals",
+      "B: Terrorize little children",
+      "C: Settle Disputes",
+      "D: Keep law and order",
+    ],
+    correct_option: "B: Terrorize little children",
   },
   {
     id: 5,
-    question: "What is the chemical symbol for gold?",
-    options: ["A: Ag", "B: Au", "C: Pt", "D: Hg"],
-    correct_option: "B: Au",
+    question: "A model of the earth is called?",
+    options: ["A: A Globe", "B: An Atlas", "C: A Map", "D: A Longitude"],
+    correct_option: "B: An Atlas",
   },
 ];
 
